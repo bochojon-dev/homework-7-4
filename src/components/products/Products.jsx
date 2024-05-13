690a568a2495abed35567a1eb962a6a43feb59c6
Bochojon-dev <maniyevmaqadjon9@gmail.com>
2024-05-13 19:15:32 +0500

    cards

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -5,6 +5,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { toggleLike } from "../../context/wishlistSlice";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import { IoCartOutline } from "react-icons/io5";
+import { FiSearch } from "react-icons/fi";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Products = ({ data }) => {
   let wishlist = useSelector((state) => state.wishlist.value);
@@ -15,23 +17,29 @@ const Products = ({ data }) => {
   const product = data?.map((el) => (
     <div key={el.id} className="card">
       <img src={el.images[0]} alt="product" />
-      {/* <div className="card_icons">
-        <img src="" alt="" />
-      </div> */}
       <div className="card_texts">
         <h3>{el.title}</h3>
         <p>${el.price}.00</p>
       </div>
-      <button onClick={() => dispatch(toggleLike(el))}>
-        {wishlist?.some((item) => item.id === el.id) ? (
-          <FaHeart />
-        ) : (
-          <FaRegHeart />
-        )}
-      </button>
-      <button>
-        <IoCartOutline />
-      </button>
+      <div className="buttons">
+        {/* <button onClick={() => dispatch(toggleLike(el))}>
+          {cart?.some((item) => item.id === el.id) ? (
+            <IoCartOutline />
+          ) : (
+            <FaShoppingCart />
+          )}
+        </button> */}
+        <button onClick={() => dispatch(toggleLike(el))}>
+          {wishlist?.some((item) => item.id === el.id) ? (
+            <FaHeart />
+          ) : (
+            <FaRegHeart />
+          )}
+        </button>
+        <button>
+          <FiSearch />
+        </button>
+      </div>
     </div>
   ));
   return (
